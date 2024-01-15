4ab8009b0456fbfb9b475a8d0b3284f14e0f08d9
Sysleec <sysleec@gmail.com>
2024-01-15 03:07:16 +0300

    design fixes

--- command_help.go
+++ command_help.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/fatih/color"
 )
 
 func commandHelp(cfg *Config, args ...string) error {
+	color.Set(color.FgHiYellow)
 	fmt.Println("Welcome to NotCoinBot!")
 	fmt.Println()
 	fmt.Println("Available commands:")
 	for _, cmd := range commands() {
 		fmt.Printf("  %s - %s\n", cmd.name, cmd.description)
 	}
+	color.Unset()
 	fmt.Println()
 	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
 type Config struct {
 }
 
 func main() {
+	color.Set(color.FgHiYellow)
+	data := []byte(`                                                                                                                                      
+            ████████████
+        ████░░░░░░░░░░░░████
+      ██░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░░░██
+    ██░░▒▒░░░░      ░░░░░░  ░░██
+  ██░░▒▒░░░░░░  ░░░░▒▒░░░░░░  ░░██ 
+  ██░░▒▒░░      ░░░░░░  ░░░░  ░░██
+  ██░░▒▒░░  ░░░░░░░░░░░░▒▒░░  ░░██
+  ██░░▒▒░░  ░░░░░░░░░░░░▒▒░░  ░░██
+  ██░░▒▒░░░░▒▒░░░░░░▒▒▒▒▒▒░░  ░░██
+  ██░░▒▒░░░░░░  ░░░░▒▒░░░░░░  ░░██
+    ██░░▒▒░░░░░░▒▒▒▒▒▒░░░░  ░░██ 
+    ██░░░░  ░░░░░░░░░░░░  ░░░░██       
+      ██░░░░            ░░░░██           
+        ████░░░░░░░░░░░░████                    
+            ████████████                           
+`)
+	fmt.Println(string(data))
+
+	color.Set(color.FgHiGreen)
+	fmt.Println("Welcome to the NotCoinBot!!!")
+	fmt.Println("For start you need to create sessions with 'create <name>'")
+	fmt.Println("For help write 'help'")
+	color.Unset()
 	cfg := Config{}
 
 	Repl(&cfg)
