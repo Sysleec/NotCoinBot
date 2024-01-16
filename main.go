036b1e975599fb64b0110478d8198284c4354760
Sysleec <sysleec@gmail.com>
2024-01-16 07:20:52 +0300

    bug fix

--- main.go
+++ main.go
@@ -32,12 +32,17 @@ func main() {
 
 	color.Set(color.FgHiGreen)
 	fmt.Println("Welcome to the NotCoinBot!!!")
+	fmt.Println()
+	fmt.Println("This bot is designed to automate the process of collecting coins in the game NotCoin")
 	fmt.Println("Author - github.com/Sysleec")
+	fmt.Println()
 	fmt.Println("You can support me with a donation to my TON wallet")
 	fmt.Println("UQDPJTL7YyUk8Jm92Vymg2X4V-pKtNdarKMyv-r1oYrcfJKX")
 	fmt.Println("And also join our community NotCoin Lions")
 	fmt.Println("https://t.me/NotcoinLions")
+	fmt.Println()
 	fmt.Println("For start you need to create sessions with 'create <name>'")
+	fmt.Println("To run the clicker, write 'start'")
 	fmt.Println("For help write 'help'")
 	color.Unset()
 	cfg := Config{}
